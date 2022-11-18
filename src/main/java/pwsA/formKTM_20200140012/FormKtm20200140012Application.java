33d7b212972d9615d31c5e75680a4e7da432ceae
Administrator <Administrator@WIN-171A8TGB90A>
2022-11-18 11:28:27 +0700

    commit 1 Signed-off-by:Administrator <Administrator@WIN-171A8TGB90A>

---
+++ src/main/java/pwsA/formKTM_20200140012/FormKtm20200140012Application.java
@@ -0,0 +1,13 @@
+package pwsA.formKTM_20200140012;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormKtm20200140012Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormKtm20200140012Application.class, args);
+	}
+
+}
---
+++ src/test/java/pwsA/formKTM_20200140012/FormKtm20200140012ApplicationTests.java
@@ -0,0 +1,13 @@
+package pwsA.formKTM_20200140012;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormKtm20200140012ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
